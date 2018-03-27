f198601aae462af483b389ac29332074d8996962
Rajkumar Purushothaman <rajkumar.purushothaman@lowes.com>
2018-03-27 15:44:09 -0400

    Working condition of the project..

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,10 +2,26 @@ var secretLetter = randomLetter();
 
 function randomLetter() {
     var allowedLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    return allowedLetters.charAt(Math.floor(Math.random*allowedLetters.length));
+    var result = allowedLetters.charAt( Math.floor(Math.random() * allowedLetters.length));
+    console.log("secret letter is " + result);
+    console.log(typeof result);  
+    return result;
+}
+
+function resetLetter() {
+	secretLetter = randomLetter();
 }
 
 
 function checkGuess(input) {
-    return secretLetter===input;
+	console.log("Comparing :) " + secretLetter + " with " + input);
+	console.log("Result  OF COMPARING" + secretLetter==input);
+	console.log(typeof secretLetter);  
+    console.log(typeof input);  
+    return secretLetter==input;
+}
+
+function keyPressed(event) {
+	var x = event.which || event.keyCode;
+	document.getElementById("guesses").innerHTML = "Your Guesses is: " + x;
 }
